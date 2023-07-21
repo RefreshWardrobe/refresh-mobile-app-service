7040ad906adec2f303a47010b953e785e279b854
Surya Shanmugaselvam <amy6zx@virginia.edu>
2023-07-21 13:20:15 -0400

    Adding extern repos, working on creating google API, using my developer account for now

--- cmd/main.go
+++ cmd/main.go
@@ -8,13 +8,26 @@ import (
 	"net"
 	"os"
 
+	"log"
+
+	//Intrnal rpos
 	"github.com/RefreshWardrobe/refresh-mobile-app-service/internal"
 	"github.com/RefreshWardrobe/refresh-mobile-app-service/pkg"
 	"github.com/hashicorp/go-hclog"
 	"google.golang.org/grpc"
+
+	//External rpos
+	"github.com/gorilla/pat"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+	"github.com/gorilla/sessions"
+
 )
 
 func main() {
+	/* Session keys, initialize Key Vault through JFrog or AWS Artifactory later */
+	gauth_key := "xxxx-56-s/453892097509qaf3462kb"  // not secure, replace with a keyvault system for getting extern API credentials
 	log := hclog.Default()
 
 	state, err := pkg.NewState(log)
@@ -34,5 +47,5 @@ func main() {
 	}
 
 	//host server to listen to tcp port packets
-	server.Server(1)
+	server.Server(l)
 }
